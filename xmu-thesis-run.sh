eca5e7d4485d40f33cb8d58a86335b280fe3ac61
Maozhong Fu <maozhongfu@gmail.com>
2025-06-28 10:14:32 +0800

    修改缩略词的实现方式，直接使用 glossaries 宏包，不再使用 glossaries-cn 宏包。后者在最新的 TexLive 版本上无法使用。符号表不再自生成，需要手动在 symbol_list.tex 里添加条目。

--- xmu-thesis-run.sh
+++ xmu-thesis-run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-BASE=demo
+BASE=main
 
 rm ${BASE}.pdf
 xelatex -no-pdf --interaction=nonstopmode ${BASE}
 bibtex ${BASE}
+# makeglossaries ${BASE}
 xelatex -no-pdf --interaction=nonstopmode ${BASE}
 xelatex --interaction=nonstopmode ${BASE}
 
